9f983f2a38b7a55a90cd127f491acfb5267629ce
Shane Ardell <shane.m.ardell@gmail.com>
2014-10-21 20:45:34 -0500

    initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,26 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+Buyster::Application.config.secret_key_base = '7c645094aff672856e6b546c563dbfd0b584501e8cc8b66c15dfd726020cc26fe5853d4acafdcf1ded9a6d7acd2c39f6381a2b5cf9e8f863481066ca7813ef58'
